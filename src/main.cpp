2f32c1636e52796cef1f3f85c7e2ed4e5f562d9a
kazama pilot <kazamapilot@gmail.com>
2025-05-23 01:09:18 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,17 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <functional>
+#include <unordered_map>
+
 /*
 git add .
 git commit --allow-empty -m "[any message]"
 git push origin master
 */
+
+using CommandHandler = std::function<void(const std::vector<std::string>&)>;
+std::unordered_map<std::string , CommandHandler> command_registry;
 
 void handle_echo(const std::vector<std::string>& tokens) {
     for (size_t i = 1; i < tokens.size(); ++i) {
@@ -15,6 +21,27 @@ void handle_echo(const std::vector<std::string>& tokens) {
     }
     std::cout << std::endl;
 }
+void handle_type(const std::vector<std::string>& tokens){
+if((auto it = command_registry.find(tokens[1]))!= command_registry.end()){
+std::cout<<it<<"is a shell "<<"builtin"<<std::endl;
+}else{
+  std::cout<<tokens[1]<<"invalid_command: not found"<<std::endl;
+}
+  
+}
+void Execute_Command(const std::string& input){
+
+    auto tokens = tokenize(input);
+    if (tokens.empty()) return;
+
+    auto it = command_registry.find(tokens[0]);
+    if (it != command_registry.end()) {
+        it->second(tokens); // Call the handler
+    } else {
+        std::cout << tokens[0] << ": command not found\n";
+    }
+}
+
 std::vector<std::string> tokenize(std::string input){
    std::istringstream iss(input);
     std::vector<std::string> tokens;
